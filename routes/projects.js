dd678f014075d69c38b11682a1e417cf3595333e
Felix Agagnier <felix_agagnier@hotmail.ca>
2019-11-20 15:02:15 -0500

    Fix PUG projects page

--- routes/projects.js
+++ routes/projects.js
@@ -1,37 +1,40 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const fetch = require("node-fetch");
-const url = "http://localhost:3000/api/projects/"
+const url = "http://localhost:3000/api/projects/";
 
-router.get('/', (req, res, next) => {
-    const headers = { headers: { Cookie: `ulang=${req.app.locals.lang}` } };
+router.get("/", (req, res, next) => {
+  const headers = { headers: { Cookie: `ulang=${req.app.locals.lang}` } };
 
-    fetch(url, headers)
-        .then(response => response.json())
-        .then(projects => {
-            console.log(projects)
-            res.render('./../views/projects', { projects: projects }, (err, body) => {
-                err ? next(err) : res.send(body);
-                
-            });
-        });
+  fetch(url, headers)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      res.render(
+        "./../views/projects",
+        { projects: data.map(d => d.project) },
+        (err, body) => {
+          err ? next(err) : res.send(body);
+        }
+      );
+    });
 });
 
-router.get('/:id', (req, res, next) => {
-	const headers = { headers: {Cookie: `ulang=${req.app.locals.lang}`} };
+router.get("/:id", (req, res, next) => {
+  const headers = { headers: { Cookie: `ulang=${req.app.locals.lang}` } };
 
-	fetch('http://localhost:3000/api/project/' + req.params.id, headers)
-		.then(response => response.json())
-		.then(project => {
-			// const projectObj = {
-			// 	project: project.project,
-			// 	publications: project.publications
-			// };
+  fetch("http://localhost:3000/api/project/" + req.params.id, headers)
+    .then(response => response.json())
+    .then(project => {
+      // const projectObj = {
+      // 	project: project.project,
+      // 	publications: project.publications
+      // };
 
-			res.render('./../views/project', projectObj, (err, body) => {
-				err ? next(err) : res.send(body);
-			});
-		});
+      res.render("./../views/project", projectObj, (err, body) => {
+        err ? next(err) : res.send(body);
+      });
+    });
 });
 
-module.exports = router
+module.exports = router;
