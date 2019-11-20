b49c687a060e60c2a01fab829ca09ef5577f3197
Émilie Dion-Paquin <emilie.dion-paquin@polymtl.ca>
2019-11-20 17:04:07 -0500

    cleaning publications

--- routes/publications.js
+++ routes/publications.js
@@ -6,55 +6,45 @@ const fetch = require('node-fetch')
 router.get('/', getPublications);
 router.post('/', savePublication, getPublications);
 
-function getPublications (req, res, next) {
-	const opts = { 
-		methods: 'GET',
-		headers: {Cookie: `ulang=${req.app.locals.lang}`}
-	};
+function getPublications(req, res, next) {
+    const fetchOpts = {
+        methods: 'GET',
+        headers: { Cookie: `ulang=${req.app.locals.lang}` }
+    };
+    fetch('http://localhost:3000/api/publications', fetchOpts)
+        .then(response => response.json())
+        .then(publications => {
+            const { limit, page, sort_by, order_by } = req.query;
 
-	fetch('http://localhost:3000/api/publications', opts)
-		.then(response => response.json())
-		.then(publications => {
-			const sortByOptions = { date: 'date', title: 'title' };
-			const orderByOptions = { asc: 'asc', desc: 'desc' };
+            const pagingOpts = {
+                pageNumber: page || 1,
+                limit: limit || 10,
+                orderBy: order_by || "desc",
+                sortBy: sort_by || "date"
+            };
+        
+            const content = {
+                publications: publications,
+                pubFormErrors: {},
+                pagingOptions: pagingOpts,
+                numberOfPages: Math.ceil(publications.length / pagingOpts.limit),
+                monthNames: moment.months()
+            };
 
-			const limit = req.query && req.query.limit ? req.query.limit : 10;	// limite par page
-			const page = req.query && req.query.page ? req.query.page : 1;		// numéro de la page du tableau
-			const sortBy = req.query && req.query.sort_by ? req.query.sort_by : sortByOptions.date;
-			const orderBy = req.query && req.query.order_by ? req.query.order_by : orderByOptions.desc;
-			const pageOpts = {
-				limit: limit,
-				pageNumber: page,
-				orderBy: orderBy,
-				sortBy: sortBy
-			};
-
-			const objForTemplate = {
-				publications: publications,
-				pubFormErrors: {},
-				pagingOptions: pageOpts,
-				numberOfPages: Math.ceil(publications.length / pageOpts.limit),
-				monthNames: moment.months()
-			};
-			console.log(objForTemplate)
-			res.render('./../views/publication', objForTemplate, (err, html) => {
-				if (err) {
-					next(err);
-				} else {
-					res.send(html);
-				}
-			});
-		});
+            res.render('./../views/publication', content, (err, body) => {
+                err ? next(err) : res.send(body);
+            });
+        });
 }
 
-function savePublication (req, res, next) {
-	const opts = { 
-		methods: 'POST',
-		headers: {Cookie: `ulang=${req.app.locals.lang}`}
-	};
+function savePublication(req, res, next) {
+    const opts = {
+        methods: 'POST',
+        headers: { Cookie: `ulang=${req.app.locals.lang}` }
+    };
 
-	fetch('http://localhost:3000/api/publications/', opts)
-		.then(response => next());
+    fetch('http://localhost:3000/api/publications/', opts)
+        .then(response => next());
 }
 
 module.exports = router
